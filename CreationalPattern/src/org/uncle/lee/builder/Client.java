f35253d9c028f5d540fc360ff19db319e0b65713
BigUncleLi <BigUncleLi@163.com>
2016-06-23 09:11:55 +0800

    refactor(deep prototype): improve showing method

--- CreationalPattern/src/org/uncle/lee/builder/Client.java
+++ CreationalPattern/src/org/uncle/lee/builder/Client.java
@@ -6,12 +6,28 @@ public class Client {
 	private static final String TAG = "builder";
 	
 	public static void main(String[] args) {
-		Builder builder1 = new ConcreteBuilder1();
 		Director director = new Director();
 		
-		director.construct(builder1);
-		Product product = builder1.getProduct();
+		Product product1 = createProduct1(director);
+		showProduct(product1);
 		
+		Product product2 = createProduct2(director);
+		showProduct(product2);
+	}
+
+	private static Product createProduct1(Director director) {
+		Builder builder1 = new ConcreteBuilder1();
+		director.construct(builder1);
+		return builder1.getProduct();
+	}
+	
+	private static Product createProduct2(Director director) {
+		Builder builder2 = new ConcreteBuilder2();
+		director.construct(builder2);
+		return builder2.getProduct();
+	}
+	
+	private static void showProduct(Product product) {
 		LogUtils.d(TAG, product.toString());
 	}
 }
