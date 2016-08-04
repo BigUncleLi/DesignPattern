5e936e92980e4a018d65a408550dddf8b691110a
BigUncleLi <BigUncleLi@163.com>
2016-08-04 23:18:03 +0800

    style(zen proxy): unchecked cast from obejct to T

--- StructuralPattern-Zen/src/org/uncle/lee/proxy/dynamic/DynamicProxy.java
+++ StructuralPattern-Zen/src/org/uncle/lee/proxy/dynamic/DynamicProxy.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 public class DynamicProxy{
+	@SuppressWarnings("unchecked")
 	public static <T> T newProxyInstance(ClassLoader loader,
 			Class<?>[] interfaces, InvocationHandler handler) {
 		return (T) Proxy.newProxyInstance(loader, interfaces, handler);
