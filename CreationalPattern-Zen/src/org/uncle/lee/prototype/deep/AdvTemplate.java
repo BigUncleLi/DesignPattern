9c5a4f0ec83562c137452c0881a8d573a76365ed
BigUncleLi <BigUncleLi@163.com>
2016-08-12 23:57:46 +0800

    feat(zen pattern): add prototpye deep pattern

---
+++ CreationalPattern-Zen/src/org/uncle/lee/prototype/deep/AdvTemplate.java
@@ -0,0 +1,14 @@
+package org.uncle.lee.prototype.deep;
+
+public class AdvTemplate {
+	private String advSubject = "raffle...";
+	private String advContext = "100,000,000 $$...";
+	
+	public String advSubject() {
+		return advSubject;
+	}
+	
+	public String advContext() {
+		return advContext;
+	}
+}
