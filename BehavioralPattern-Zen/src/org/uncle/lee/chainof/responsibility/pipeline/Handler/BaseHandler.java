36e2832fadb247d2364feaa6edd7d7e9a915b3e3
BigUncleLi <BigUncleLi@163.com>
2016-08-18 21:28:46 +0800

    refactor(zen chain): improve simple demo
    Adding thread pool for handler to do action.

--- BehavioralPattern-Zen/src/org/uncle/lee/chainof/responsibility/pipeline/Handler/BaseHandler.java
+++ BehavioralPattern-Zen/src/org/uncle/lee/chainof/responsibility/pipeline/Handler/BaseHandler.java
@@ -1,5 +1,8 @@
 package org.uncle.lee.chainof.responsibility.pipeline.Handler;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.uncle.lee.chainof.responsibility.pipeline.request.Request;
 import org.uncle.lee.chainof.responsibility.pipeline.request.Request.CurrentBoss;
 import org.uncle.lee.chainof.responsibility.pipeline.response.ResponseListener;
@@ -8,26 +11,49 @@ import org.uncle.lee.utils.LogUtils;
 public abstract class BaseHandler implements Handler, Comparable<BaseHandler> {
 	private int priority;
 	protected CurrentBoss my;
+	private Request request;
+	private ResponseListener responseListener;
+	
+	private ExecutorService threadPool;
 
 	public BaseHandler(int priority) {
 		this.priority = priority;
 		my = initCurrentBoss();
+		initThreadPool();
+	}
+
+	private void initThreadPool() {
+		threadPool = Executors.newCachedThreadPool();
 	}
 
 	public boolean handleRequest(Request request, ResponseListener responseListener) {
-		if (needHandle(request)) {
-			doHandle(request, responseListener);
+		this.request = request;
+		this.responseListener = responseListener;
+		
+		if (needHandle()) {
+			doHandleInThread();
 			return true;
 		} else {
 			return false;
 		}
 	}
 
-	public boolean needHandle(Request request) {
+	public boolean needHandle() {
 		boolean need = my != null && request.getCurrentBoss().equals(my);
 		showNeedHandleInfo(need);
 		return need;
 	}
+	
+	private void doHandleInThread() {
+		threadPool.submit(task);
+	}
+	
+	private Runnable task = new Runnable() {
+		@Override
+		public void run() {
+			doHandle(request, responseListener);
+		}
+	};
 
 	private void showNeedHandleInfo(boolean need) {
 		if (need) {
