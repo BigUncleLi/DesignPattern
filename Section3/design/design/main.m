42e739a85a97f76660778f9fbe4a7c5a047387ea
BigUncleLi <BigUncleLi@163.com>
2017-07-22 11:22:24 +0800

    chore : add desgin partten oc implement project

---
+++ Section3/design/design/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  design
+//
+//  Created by AustinLee on 22/07/2017.
+//  Copyright © 2017 lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
