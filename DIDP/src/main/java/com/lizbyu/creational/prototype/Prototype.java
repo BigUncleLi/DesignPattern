af42335a7c1692deea40f76869e61b95c8f7847a
Uybzil <lee.cleancoder@gmail.com>
2020-07-14 23:33:55 +0800

    feat: add prototype pattern

---
+++ DIDP/src/main/java/com/lizbyu/creational/prototype/Prototype.java
@@ -0,0 +1,36 @@
+package com.lizbyu.creational.prototype;
+
+public class Prototype implements Cloneable {
+    private int intValue;
+    private String strValue;
+    private Object innerClass;
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public void setIntValue(int intValue) {
+        this.intValue = intValue;
+    }
+
+    public String getStrValue() {
+        return strValue;
+    }
+
+    public void setStrValue(String strValue) {
+        this.strValue = strValue;
+    }
+
+    public Object getInnerClass() {
+        return innerClass;
+    }
+
+    public void setInnerClass(Object innerClass) {
+        this.innerClass = innerClass;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
---
+++ DIDP/src/test/java/com/lizbyu/creational/prototype/Client.java
@@ -0,0 +1,33 @@
+package com.lizbyu.creational.prototype;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Client {
+
+    @Test
+    public void testClient() {
+        int intValue = 0;
+        String strValue = "test-str";
+        Object obj = new Object();
+
+        Prototype prototype = new Prototype();
+        prototype.setIntValue(intValue);
+        prototype.setStrValue(strValue);
+        prototype.setInnerClass(obj);
+
+        try {
+            Prototype clonePrototype = (Prototype) prototype.clone();
+            assertEquals(intValue, clonePrototype.getIntValue());
+            assertEquals(strValue, clonePrototype.getStrValue());
+            assertEquals(obj, clonePrototype.getInnerClass());
+
+            assertSame(intValue, clonePrototype.getIntValue());
+            assertSame(strValue, clonePrototype.getStrValue());
+            assertSame(obj, clonePrototype.getInnerClass());
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+    }
+}
